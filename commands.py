496ae424628a5c460d3033399830b942aa2098eb
LeVeDy22 <kostyapysmenny2@gmail.com>
2024-10-15 15:11:32 +0300

    add commands.py

---
+++ commands.py
@@ -0,0 +1,21 @@
+command_dict = {
+    "commands": {
+        "greeting": ["привет", "приветствую", "hello", "hi"],
+        "create_task": [
+            "добавить задачу",
+            "создать задачу",
+            "заметка",
+            "create a task",
+            "add a task",
+        ],
+        "open_browser": ["открыть браузер", "open browser"],
+        "close_browser": ["закрыть браузер", "close browser"],
+        "open_youtube": ["открыть ютуб", "открыть youtube", "open youtube"],
+        "restart": ["перезагрузить ноутбук", "restart"],
+        "shutdown": ["выключить ноутбук", "shutdown"],
+        "exit_program": ["закрыть программу", "выход", "exit"],
+        "open_mindustry": ["открыть миндастри", "открыть mindustry", "open mindustry"],
+        "open_roblox": ["открыть роблокс", "открыть roblox", "open roblox"],
+        "calculator": ["калькулятор", "calculator"],
+    }
+}
